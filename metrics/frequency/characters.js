30fa190b4de0f842772d01e2a5e8f7457771bd13
Andrew Brown <drusepth@gmail.com>
2014-05-05 23:28:57 +0000

    metrics for character-based analysis

--- metrics/frequency/characters.js
+++ metrics/frequency/characters.js
@@ -1,23 +1,33 @@
-function spacing_frequency(text) {
+load("lib/frequency.js");
 
+function spacing_frequency(text) {
+  var space_types = [' ', "\t", "\r", "\n"];
+  return frequency_of_chars_in_string(space_types, text) / text.length;
 }
 
 function vowel_frequency(text) {
-
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  return frequency_of_chars_in_string(vowels, text) / text.length;
 }
 
 function consonant_frequency(text) {
-
+  var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p',
+                    'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+  return frequency_of_chars_in_string(consonants, text) / text..length;
 }
 
 function digit_frequency(text) {
-
+  var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  return frequency_of_chars_in_string(digits, text) / text.length;
 }
 
 function punctuation_frequency(text) {
-
+  var punctuation = ['.', ',', ';', '?', '!', '-', '"', "'", ':', '(', ')', '[', ']'];
+  return frequency_of_chars_in_string(punctuation, text) / text.length;
 }
 
 function special_character_frequency(text) {
+  var special = ['$', '^', '#', '@', '^', '%', '~'];
+  return frequency_of_chars_in_string(special, text) / text.length;
 
 }
